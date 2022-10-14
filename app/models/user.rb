e7e15b694ae719823a655ca15f694b40b472940c
Camilo Botero <cbotero2709@gmail.com>
2022-10-14 11:31:13 -0500

    email and pass validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validate :password_regex
+
   has_many :reviews
   has_many :bookings
   has_many :services
@@ -7,4 +9,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  private
+
+  def password_regex
+    return if password.blank? || password =~ /\A(?=.*\d)(?=.*[A-Z])(?=.*\W)[^ ]{6,}\z/
+
+    errors.add :password, 'Password should have more than 6 characters including 1 uppercase letter, 1 number, 1 special character'
+  end
 end
