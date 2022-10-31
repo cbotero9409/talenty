70d51dee3cbca6726a6419f01f5f9e23c9b77ea4
Camilo Botero <cbotero2709@gmail.com>
2022-10-31 14:25:24 -0500

    homepage

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,21 +1,21 @@
-// Entry point for the build script in your package.json
+//Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
 
-// let xOffset = 0;
-// let isMouseIn = false;
-// const slides = document.getElementById("slides");
-// setInterval(translate, 0);
-// function translate() {
-//   let offsetIncrementor = isMouseIn ? 0.05 : 0.2;
-//   if (xOffset >= 258 * 7) xOffset = 0;
-//   else xOffset = xOffset + offsetIncrementor;
-//   slides.style.transform = "translateX(-" + xOffset + "px)";
-// }
-// slides.addEventListener("mouseover", function (event) {
-//   isMouseIn = true;
-// });
-// slides.addEventListener("mouseout", function (event) {
-//   isMouseIn = false;
-// });
+let xOffset = 0;
+let isMouseIn = false;
+const slides = document.getElementById("slides");
+setInterval(translate, 0);
+function translate() {
+  let offsetIncrementor = isMouseIn ? 0.05 : 0.2;
+  if (xOffset >= 258 * 7) xOffset = 0;
+  else xOffset = xOffset + offsetIncrementor;
+  slides.style.transform = "translateX(-" + xOffset + "px)";
+}
+slides.addEventListener("mouseover", function (event) {
+  isMouseIn = true;
+});
+slides.addEventListener("mouseout", function (event) {
+  isMouseIn = false;
+});
