ac03504850f0e72c7f62227b57506f80e65d8001
jan diaz <jancarlosdiaz26@gmail.com>
2022-11-04 16:13:33 -0500

    seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :services
   has_one_attached :photo
+  has_many_attached :photos_services
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
